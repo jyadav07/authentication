7f2b7bd76ed60b42cd4d534045d33a1d70b97e05
Jaya Yadav <jaya.yadav@kellogg.northwestern.edu>
2025-05-20 16:22:33 +0000

    more changes

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,12 +1,18 @@
 class ActivitiesController < ApplicationController
 
   def create
+    @user = User.findby({"id" => session["user_id"]})
+    if @user != nil
     @activity = Activity.new
     @activity["contact_id"] = params["contact_id"]
     @activity["activity_type"] = params["activity_type"]
     @activity["note"] = params["note"]
+    @activity["user_id"] = session["user_id"]
     @activity.save
     redirect_to "/contacts/#{@activity["contact_id"]}"
+    else 
+      redirect_to "/login"
+    end
   end
 
 end
